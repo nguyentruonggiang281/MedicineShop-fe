0296804db6c1e1a19ef270ba4dbd39e1cfcfc6d8
NGUYỄN TRƯỜNG GIANG <91778158+nguyentruonggiang281@users.noreply.github.com>
2023-03-30 22:22:37 +0700

    Delete _mock directory

--- _mock/account.js
+++
@@ -1,9 +0,0 @@
-// ----------------------------------------------------------------------
-
-const account = {
-  displayName: 'Nguyễn Trường Giang',
-  email: 'nguyengianga32001@gmail.com',
-  photoURL: '/assets/images/avatars/avatar_default.jpg',
-};
-
-export default account;
