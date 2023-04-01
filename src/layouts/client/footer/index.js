2e2fdfe358e95e0dfed2f88685af6e5dee10f04f
NGUYỄN TRƯỜNG GIANG <91778158+nguyentruonggiang281@users.noreply.github.com>
2023-04-01 16:21:29 +0700

    Update index.js

--- src/layouts/client/footer/index.js
+++ src/layouts/client/footer/index.js
@@ -1,34 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import { Box, Typography } from '@mui/material';
+import { Typography, Grid } from "@mui/material";
+import React from "react";
 
-const Footer = () => {
-  const [isVisible, setVisible] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollY = window.scrollY || window.pageYOffset;
-      const visibleThreshold = document.documentElement.offsetHeight - window.innerHeight;
-      setVisible(scrollY > visibleThreshold);
-    };
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
+function Footer() {
   return (
-    <Box sx={{
-      position: 'fixed',
-      
-      width: '100%',
-      bgcolor: 'background.paper',
-      p: 2,
-      bottom: isVisible ? 0 : -100,
-      transition: 'bottom 0.3s ease-in-out',
-    }}>
-      <Typography>asuhoahskj kasjh</Typography>
-    </Box>
+    <footer
+      style={{
+        background: "#f0f",
+        width: "100%",
+        height: "200px",
+        margin: "24px 0 0 -16px",
+        padding: "0px",
+        position: "absolute",
+      }}
+    >
+      <Grid container sx={{ bgcolor: 'lightgrey', py: 3 }}>
+      {/* Cột đầu tiên */}
+      <Grid item xs={12} md={4} >
+        <Typography variant="h5" sx={{ fontWeight: 'bold' }}>Cột đầu tiên</Typography>
+      </Grid>
+      {/* Các cột còn lại */}
+      <Grid item xs={12} md={2} >
+        <Typography variant="subtitle1">Cột 2</Typography>
+      </Grid>
+      <Grid item xs={12} md={2} >
+        <Typography variant="subtitle1">Cột 3</Typography>
+      </Grid>
+      <Grid item xs={12} md={2} >
+        <Typography variant="subtitle1">Cột 4</Typography>
+      </Grid>
+      <Grid item xs={12} md={2} >
+        <Typography variant="subtitle1">Cột 5</Typography>
+      </Grid>
+    </Grid>
+    </footer>
   );
-};
+}
 
 export default Footer;
