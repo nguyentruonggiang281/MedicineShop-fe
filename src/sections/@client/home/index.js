00aaccef8202afa008cd1d7072ba663a19b6edb6
NGUYỄN TRƯỜNG GIANG <91778158+nguyentruonggiang281@users.noreply.github.com>
2023-04-01 13:22:21 +0700

    Delete src/sections directory

--- src/sections/@client/home/index.js
+++
@@ -1,2 +0,0 @@
-export { default as SimpleSlider } from './SimpleSliders';
-export { default as SearchForm } from './SearchForm';
--- src/sections/@client/products/index.js
+++
@@ -1,3 +0,0 @@
-export { default as ProductCard } from './ProductCard';
-export { default as ProductCartWidget } from './ProductCartWidget';
-export { default as ProductList } from './ProductList';
